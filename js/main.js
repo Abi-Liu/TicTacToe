14a660d05f384642879d8b21ed162f33f1809c6b
Abi Liu <abiliu018@gmail.com>
2022-12-29 17:59:36 -0800

    adding game functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-let player = 'X'
+const squares = document.querySelectorAll('.square')
+squares.forEach(square => {
+    square.addEventListener('click', placeLetter)
+})
+class Board{
+    constructor(){
 
+    }
+
+}
+
+class Player{
+    constructor(){
+
+    }
+    
+}
+
+let player = "X";
 
 function changePlayer() {
     if(player == 'X') {
