6aaae7f98aa6d5713cb9cb972c970a970c9094f1
Abi Liu <abiliu018@gmail.com>
2022-12-29 18:11:36 -0800

    changes

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
-const squares = document.querySelectorAll('.square')
-squares.forEach(square => {
-    square.addEventListener('click', placeLetter)
-})
-class Board{
-    constructor(){
+// const squares = document.querySelectorAll('.square')
+// squares.forEach(square => {
+//     square.addEventListener('click', placeLetter)
+// })
+// class Board{
+//     constructor(){
 
-    }
+//     }
 
-}
+// }
 
-class Player{
-    constructor(){
+// class Player{
+//     constructor(letter){
+//         this._letter = letter
+//     }
+//     get letter(){
+//         return this._letter
+//     }
+// }
 
-    }
-    
-}
+// let player = "X";
 
-let player = "X";
-
-function changePlayer() {
-    if(player == 'X') {
-        player = 'O'
-    } else {
-        player = 'X'
-    }
-}
+// function changePlayer() {
+//     if(player == 'X') {
+//         player = 'O'
+//     } else {
+//         player = 'X'
+//     }
+// }
+console.log('hi')
