5222eb0d638ec68078315c6101584ff2d13c9ffa
Dan <DanRHatfield@gmail.com>
2022-12-29 20:48:12 -0700

    updated layout

--- js/main.js
+++ js/main.js
@@ -1,47 +1,4 @@
-// const squares = document.querySelectorAll('.square')
 
-// class Board{
-//     constructor(){
-        
-//     }
-//     initialize(){
-//         squares.forEach((square) => {
-//           square.addEventListener("click", placeLetter());
-//         });
-//     }
-
-//     placeLetter(letter){
-        
-//     }
-// }
-
-// class Player{
-//     constructor(letter){
-//         this._letter = letter
-//     }
-//     get letter(){
-//         return this._letter
-//     }
-// }
-
-// // function placeLetter () {
-    
-// //     console.log("square")
-// // }
-
-// let player = "X";
-
-// function changePlayer() {
-//     if(player == 'X') {
-//         player = 'O'
-//     } else {
-//         player = 'X'
-//     }
-// }
-
-// console.log('hi')
-
-// let theGameBegin = new Board
 let count = 0
 let won = false
 const squares = document.querySelectorAll('.square')
@@ -50,8 +7,6 @@ const squares = document.querySelectorAll('.square')
     });
 
 let player = "X"
-
-// document.getElementById("one").addEventListener('click', updateSquare)
 
 function updateSquare() {
     if(won){
